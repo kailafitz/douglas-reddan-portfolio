bfac2377850706b5e865a35b3eae505f61342978
kailafitz <mikhailafitzpatrick@yahoo.com>
2023-12-19 02:41:23 +0000

    Nav link test animations

--- components/custom/navLinks.tsx
+++ components/custom/navLinks.tsx
@@ -4,16 +4,28 @@ import React from "react";
 const NavLinks = () => {
   return (
     <div className="flex flex-col lg:flex-row align-baseline flex-grow justify-end pt-8 lg:pt-0">
-      <Link href="#work" className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg">
+      <Link
+        href="#work"
+        className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg btn-animation"
+      >
         Work
       </Link>
-      <Link href="#bio" className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg">
+      <Link
+        href="#bio"
+        className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg btn-animation"
+      >
         Bio
       </Link>
-      <Link href="#press" className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg">
+      <Link
+        href="#press"
+        className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg btn-animation"
+      >
         Press
       </Link>
-      <Link href="#contact" className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg">
+      <Link
+        href="#contact"
+        className="ml-8 mb-3 lg:mb-0 text-xl lg:text-lg btn-animation"
+      >
         Contact
       </Link>
     </div>
